ed229fa9839ef50e77d39e7ea465179bf2e4ab18
Imali-98 <imalimadhubhashini98@gmail.com>
2023-07-05 07:33:50 +0530

    Code Refactoring

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,11 @@ import TodosView from '../views/TodosView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: TodosView
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: TodosView
+    // },
     {
       path: '/about',
       name: 'about',
